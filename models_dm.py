10d9d52b38b0930492a0da326382c0409702b65a
williamxieigs <yi.xie@igsl-group.com>
2025-10-09 16:13:03 +0800

    update

--- models_dm.py
+++ models_dm.py
@@ -212,5 +212,5 @@ class DiscreteSAC(nn.Module):
     return self.Q(states, actions)
   def get_qs(self, new_states, rewards, dones, gamma):
     # new_states.shape = (batch, stack_length, h, w) rewrads.shape = (batch,) dones.shape = (batch)
-    vs = rewards + gamma * torch.where(dones > 0.5, torch.zeros_like(rewards), torch.ones_like(rewrads)) * self.V(new_states)
+    vs = rewards + gamma * torch.where(dones > 0.5, torch.zeros_like(rewards), torch.ones_like(rewards)) * self.V(new_states)
     return vs.detach()
